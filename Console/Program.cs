6a23b6d372fed7bce7f20b3746011d08fe8079b2
Ben Stolovitz <ben@stolovitz.com>
2025-11-05 18:50:31 -0500

    initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+unsafe
+{
+    PInvoke.EnumWindows((hwnd, lParam) =>
+    {
+        const int maxLength = 256;
+        Span<char> buffer = stackalloc char[maxLength];
+            
+        fixed (char* pBuffer = buffer)
+        {
+            var pwstr = new PWSTR(pBuffer);
+            int length = PInvoke.GetWindowText(hwnd, pwstr, maxLength);
+            string title = length > 0 ? new string(pBuffer, 0, length) : string.Empty;
+            Console.WriteLine($"HWND: 0x{hwnd:X}, Title: '{title}'");
+        }
+            
+        return true; // continue enumeration
+    }, IntPtr.Zero);
+}
