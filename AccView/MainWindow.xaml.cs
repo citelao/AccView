0dc75a71511e828b13fb1257acbb7f0381d08875
Ben Stolovitz <ben@stolovitz.com>
2025-11-05 19:36:16 -0500

    ui

--- AccView/MainWindow.xaml.cs
+++ AccView/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Shared;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -26,6 +27,30 @@ namespace AccView
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var uia = UIAHelpers.CreateUIAutomationInstance();
+            var root = uia.GetRootElement();
+            var condition = uia.CreateTrueCondition();
+
+            var children = root.FindAll(Windows.Win32.UI.Accessibility.TreeScope.TreeScope_Children, condition);
+            for (int i = 0; i < children.Length; i++)
+            {
+                var element = children.GetElement(i);
+                var name = (string)element.GetCurrentPropertyValue(Windows.Win32.UI.Accessibility.UIA_PROPERTY_ID.UIA_NamePropertyId);
+                var automationId = (string)element.GetCurrentPropertyValue(Windows.Win32.UI.Accessibility.UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
+                // Check to see if this element has children
+                var childCondition = uia.CreateTrueCondition();
+                var childElements = element.FindFirst(Windows.Win32.UI.Accessibility.TreeScope.TreeScope_Children, childCondition);
+                var hasChildren = childElements != null;
+                OutputTextBlock.Text += $"{i}: Name='{name}', AutomationId='{automationId}'\r\n";
+                if (hasChildren)
+                {
+                    OutputTextBlock.Text += "   + [...]\r\n";
+                }
+            }
         }
     }
 }
