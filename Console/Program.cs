2938193b7bd3603d1743cab46d1c28e9e4660a43
Ben Stolovitz <ben@stolovitz.com>
2025-11-05 19:25:42 -0500

    children!

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,24 +6,24 @@ using Windows.Win32.UI.WindowsAndMessaging;
 
 using Windows.Win32.UI.Accessibility;
 
-unsafe
-{
-    PInvoke.EnumWindows((hwnd, lParam) =>
-    {
-        const int maxLength = 256;
-        Span<char> buffer = stackalloc char[maxLength];
+//unsafe
+//{
+//    PInvoke.EnumWindows((hwnd, lParam) =>
+//    {
+//        const int maxLength = 256;
+//        Span<char> buffer = stackalloc char[maxLength];
             
-        fixed (char* pBuffer = buffer)
-        {
-            var pwstr = new PWSTR(pBuffer);
-            int length = PInvoke.GetWindowText(hwnd, pwstr, maxLength);
-            string title = length > 0 ? new string(pBuffer, 0, length) : string.Empty;
-            Console.WriteLine($"HWND: 0x{hwnd:X}, Title: '{title}'");
-        }
+//        fixed (char* pBuffer = buffer)
+//        {
+//            var pwstr = new PWSTR(pBuffer);
+//            int length = PInvoke.GetWindowText(hwnd, pwstr, maxLength);
+//            string title = length > 0 ? new string(pBuffer, 0, length) : string.Empty;
+//            Console.WriteLine($"HWND: 0x{hwnd:X}, Title: '{title}'");
+//        }
 
-        return true; // continue enumeration
-    }, IntPtr.Zero);
-}
+//        return true; // continue enumeration
+//    }, IntPtr.Zero);
+//}
 
 // List all top-level windows with their titles
 var automation = CreateUIAutomationInstance();
@@ -35,7 +35,18 @@ for (int i = 0; i < children.Length; i++)
     var element = children.GetElement(i);
     var name = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId);
     var automationId = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
+
+    // Check to see if this element has children
+    var childCondition = automation.CreateTrueCondition();
+    var childElements = element.FindFirst(TreeScope.TreeScope_Children, childCondition);
+    var hasChildren = childElements != null;
+
     Console.WriteLine($"{i}: Name='{name}', AutomationId='{automationId}'");
+    if (hasChildren)
+    {
+        Console.WriteLine("   + [...]");
+    }
+
 }
 
 static IUIAutomation CreateUIAutomationInstance()
