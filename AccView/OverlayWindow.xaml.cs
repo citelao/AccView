52314fe0c2833ae2f365e5600938fa4cd71f4170
Ben Stolovitz <ben@stolovitz.com>
2025-11-06 22:17:32 -0500

    window...

---
+++ AccView/OverlayWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace AccView;
+
+public sealed partial class OverlayWindow : WinUIEx.WindowEx
+{
+    public OverlayWindow()
+    {
+        InitializeComponent();
+    }
+}
