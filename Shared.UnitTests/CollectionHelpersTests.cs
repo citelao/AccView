a34b9d3a33ee9e8d376a4510d4bf7098256648b2
Ben Stolovitz <ben@stolovitz.com>
2025-11-11 18:46:12 -0500

    run tests

---
+++ Shared.UnitTests/CollectionHelpersTests.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Shared.UnitTests;
+
+public class CollectionHelpersTests
+{
+    [Fact]
+    public void MergesProperly()
+    {
+        var ob = new ObservableCollection<string>() { "a", "b", "c" };
+    }
+}
