5237a5f291d4223d4fbd2fbcd4a452a264d3de52
Ben Stolovitz <ben@stolovitz.com>
2025-11-12 13:57:42 -0500

    list tree...

---
+++ Cmd/Commands/TreeCommands.cs
@@ -0,0 +1,35 @@
+﻿using Shared;
+using Windows.Win32.UI.Accessibility;
+
+namespace Cmd.Commands
+{
+    public class TreeCommands
+    {
+        public static void ListTopLevelTree()
+        {
+            // List all top-level windows with their titles
+            var automation = UIAHelpers.CreateUIAutomationInstance();
+            var root = automation.GetRootElement();
+            var condition = automation.CreateTrueCondition();
+            var children = root.FindAll(TreeScope.TreeScope_Children, condition);
+            for (int i = 0; i < children.Length; i++)
+            {
+                var element = children.GetElement(i);
+                var name = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId);
+                var automationId = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
+
+                // Check to see if this element has children
+                var childCondition = automation.CreateTrueCondition();
+                var childElements = element.FindFirst(TreeScope.TreeScope_Children, childCondition);
+                var hasChildren = childElements != null;
+
+                Console.WriteLine($"{i}: Name='{name}', AutomationId='{automationId}'");
+                if (hasChildren)
+                {
+                    Console.WriteLine("   + [...]");
+                }
+            }
+
+        }
+    }
+}
--- Cmd/Program.cs
+++ Cmd/Program.cs
@@ -1,35 +1,9 @@
-﻿using Windows.Win32.UI.Accessibility;
-using Shared;
-using System.CommandLine;
-using System.CommandLine.Invocation;
+﻿using System.CommandLine;
 
 var program = CreateCommand();
 
 // Execute the command line parser
 return await program.Parse(args).InvokeAsync();
-
-// List all top-level windows with their titles
-var automation = UIAHelpers.CreateUIAutomationInstance();
-var root = automation.GetRootElement();
-var condition = automation.CreateTrueCondition();
-var children = root.FindAll(TreeScope.TreeScope_Children, condition);
-for (int i = 0; i < children.Length; i++)
-{
-    var element = children.GetElement(i);
-    var name = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId);
-    var automationId = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
-
-    // Check to see if this element has children
-    var childCondition = automation.CreateTrueCondition();
-    var childElements = element.FindFirst(TreeScope.TreeScope_Children, childCondition);
-    var hasChildren = childElements != null;
-
-    Console.WriteLine($"{i}: Name='{name}', AutomationId='{automationId}'");
-    if (hasChildren)
-    {
-        Console.WriteLine("   + [...]");
-    }
-}
 
 static RootCommand CreateCommand()
 {
@@ -45,9 +19,22 @@ static RootCommand CreateCommand()
         listWindowsCommand,
     };
 
+    var listTreeCommand = new Command("list", "List all top-level UI Automation elements")
+    {
+    };
+    listTreeCommand.SetAction((parse) =>
+    {
+        Cmd.Commands.TreeCommands.ListTopLevelTree();
+    });
+    var treeCommands = new Command("tree", "UIA tree tools")
+    {
+        listTreeCommand,
+    };
+
     var program = new RootCommand("Simple accessibility tools")
     {
         windowsCommands,
+        treeCommands,
     };
     return program;
 }
