d7ee3f940c3d13cdf9a704195bf1c5e41b5a2b10
Ben Stolovitz <ben@stolovitz.com>
2025-11-18 18:17:29 -0500

    it doesn't work

--- Cmd/Commands/EventCommands.cs
+++ Cmd/Commands/EventCommands.cs
@@ -25,20 +25,23 @@ namespace Cmd.Commands
                 cache.AddProperty(UIA_PROPERTY_ID.UIA_LocalizedControlTypePropertyId);
                 cache.AddProperty(UIA_PROPERTY_ID.UIA_RuntimeIdPropertyId);
 
-                IUIAutomationElement? consoleUiaElement = null;
-                var windowHandle = PInvoke.GetConsoleWindow();
-                if (windowHandle == IntPtr.Zero)
-                {
-                    Console.WriteLine(Red("Failed to get console window handle."));
-                }
-                else
-                {
-                    consoleUiaElement = uia.ElementFromHandle(windowHandle);
-                    if (consoleUiaElement == null)
-                    {
-                        Console.WriteLine(Red("Failed to get UIA element from console window handle."));
-                    }
-                }
+                // This doesn't work for Windows Terminal: GetConsoleWindow returns a proxy HWND.
+                // https://github.com/microsoft/terminal/blob/19a85010fe23d486dbc7b832c9c1e54069a1b233/doc/specs/%2312570%20-%20Show%20Hide%20operations%20on%20GetConsoleWindow%20via%20PTY.md
+                //
+                //IUIAutomationElement? consoleUiaElement = null;
+                //var windowHandle = PInvoke.GetConsoleWindow();
+                //if (windowHandle == IntPtr.Zero)
+                //{
+                //    Console.WriteLine(Red("Failed to get console window handle."));
+                //}
+                //else
+                //{
+                //    consoleUiaElement = uia.ElementFromHandle(windowHandle);
+                //    if (consoleUiaElement == null)
+                //    {
+                //        Console.WriteLine(Red("Failed to get UIA element from console window handle."));
+                //    }
+                //}
 
                 var focusHandler = new FocusChangedEventHandler();
                 focusHandler.FocusChanged += (sender, e) =>
@@ -78,24 +81,25 @@ namespace Cmd.Commands
                     var element = e.Sender;
 
                     // Avoid notifications from children of our own console window
-                    if (consoleUiaElement != null)
-                    {
-                        var consoleRuntimeId = (int[])consoleUiaElement.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_RuntimeIdPropertyId);
-                        var bound = consoleUiaElement.CurrentBoundingRectangle;
-                        var currentAncestor = element;
-                        var walker = uia.RawViewWalker;
-                        while (currentAncestor != null)
-                        {
-                            var ancestorRuntimeId = (int[])currentAncestor.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_RuntimeIdPropertyId);
-                            Console.WriteLine($"ancestor: {string.Join(",", ancestorRuntimeId)} {currentAncestor.CurrentLocalizedControlType} vs {consoleUiaElement.CurrentLocalizedControlType} {string.Join(",", consoleRuntimeId)} {bound.left},{bound.top},{bound.right},{bound.bottom}");
-                            if (uia.CompareElements(currentAncestor, consoleUiaElement))
-                            {
-                                // Ignoring notification from console window
-                                return;
-                            }
-                            currentAncestor = walker.GetParentElement(currentAncestor);
-                        }
-                    }
+                    // Doesn't work; see above.
+                    //if (consoleUiaElement != null)
+                    //{
+                    //    var consoleRuntimeId = (int[])consoleUiaElement.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_RuntimeIdPropertyId);
+                    //    var bound = consoleUiaElement.CurrentBoundingRectangle;
+                    //    var currentAncestor = element;
+                    //    var walker = uia.RawViewWalker;
+                    //    while (currentAncestor != null)
+                    //    {
+                    //        var ancestorRuntimeId = (int[])currentAncestor.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_RuntimeIdPropertyId);
+                    //        Console.WriteLine($"ancestor: {string.Join(",", ancestorRuntimeId)} {currentAncestor.CurrentLocalizedControlType} vs {consoleUiaElement.CurrentLocalizedControlType} {string.Join(",", consoleRuntimeId)} {bound.left},{bound.top},{bound.right},{bound.bottom}");
+                    //        if (uia.CompareElements(currentAncestor, consoleUiaElement))
+                    //        {
+                    //            // Ignoring notification from console window
+                    //            return;
+                    //        }
+                    //        currentAncestor = walker.GetParentElement(currentAncestor);
+                    //    }
+                    //}
 
                     var name = (string)element.GetCachedPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId);
                     var automationId = (string)element.GetCachedPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
