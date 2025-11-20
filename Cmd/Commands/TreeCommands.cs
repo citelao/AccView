b56d01ccb303a4224a4ccc22d387a85adc1934fd
Jevan Saks <jevansa@microsoft.com>
2025-11-19 16:17:04 -0800

    Switch to source generated COM

--- Cmd/Commands/TreeCommands.cs
+++ Cmd/Commands/TreeCommands.cs
@@ -1,5 +1,6 @@
 ﻿using Shared;
 using Windows.Win32.UI.Accessibility;
+using WinRT;
 
 namespace Cmd.Commands
 {
@@ -12,11 +13,11 @@ namespace Cmd.Commands
             var root = automation.GetRootElement();
             var condition = automation.CreateTrueCondition();
             var children = root.FindAll(TreeScope.TreeScope_Children, condition);
-            for (int i = 0; i < children.Length; i++)
+            for (int i = 0; i < children.get_Length(); i++)
             {
                 var element = children.GetElement(i);
-                var name = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId);
-                var automationId = (string)element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
+                var name = element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_NamePropertyId).As<string>();
+                var automationId = element.GetCurrentPropertyValue(UIA_PROPERTY_ID.UIA_AutomationIdPropertyId);
 
                 // Check to see if this element has children
                 var childCondition = automation.CreateTrueCondition();
